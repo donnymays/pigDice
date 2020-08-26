7d14bff00619f97433f03b5568707bbd8bedb347
jbnilles <jbnilles24@gmail.com>
2020-08-26 11:57:20 -0700

    add js functionality to app
    Co-authored-by: Donny Mays <dtmays2@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-let test = new Game();
+let GAME = new Game();
 let DIE = 0
 
 function Game () {
 this.players = []; //[tom, bob]
-this.playerIndex;//   0     1
+this.currentPlayerIndex = 0;//   0     1
 this.playerCount = 0;
 //this.gameVariationList = ['default', '2dice'];
 //this.gameVariationSelected = default
@@ -14,45 +14,133 @@ Game.prototype.addPlayer = function (player) {
   this.playerCount++;
 }
 Game.prototype.nextPlayer = function () {
-  this.players[this.playerIndex].turnScore = 0;
-  if(this.playerIndex + 1 < this.playerCount) {
-    this.playerIndex++;
+  this.players[this.currentPlayerIndex].turnScore = 0;
+  this.players[this.currentPlayerIndex].numberTurns++;
+  if(this.currentPlayerIndex + 1 < this.playerCount) {
+    this.currentPlayerIndex++;
   } else {
-    this.playerIndex = 0;
+    this.currentPlayerIndex = 0;
   }
 }
-Game.prototype.turn = function () {
-  this.players[this.playerIndex].turn();
+Game.prototype.resetPlayers = function () {
+  this.players.forEach(element => {
+    element.resetPlayer();
+  });
 }
+Game.prototype.resetGame = function () {
+  this.currentPlayerIndex = 0;
+  this.resetPlayers();
+}
+Game.prototype.rollDice = function() {
+  return Math.floor(Math.random() * 6) + 1; 
+}
+Game.prototype.turn = function () {
+  
+  if(DICE === 1) {
+    return false;
+  } else {
+    this.players[GAME.currentPlayerIndex].turnScore += DICE;
+    return true;
+  }
 
-
+}
 
 
 function Player (name) {
   this.name = name;
   this.score = 0;
   this.turnScore = 0;
+  this.numberTurns = 0;
   //this.isHumen = true
 }
-Player.prototype.rollDice = function() {
-  return Math.floor(Math.random() * 6) + 1; 
+Player.prototype.resetPlayer = function () {
+  this.score = 0;
+  this.turnScore = 0;
+  this.numberTurns = 0;
 }
-Player.prototype.turn = function () {
-  
-  if(DIE === 1) {
-    return false;
-  } else {
-    this.turnScore += dice;
-    return true;
-  }
-}
+
 Player.prototype.submitTurnScore = function () {
   this.score += this.turnScore;
 }
+//dom = $('#playerScoreList')
+function updatePlayerListScore (dom) {
+  dom.text('');
+  for(let i = 0; i < GAME.playerCount; i++) {
+    if(i === GAME.currentPlayerIndex) {
+      dom.append('<li><strong>' + GAME.players[i].name + ': ' + GAME.players[i].score + '</strong></li>');
+    }else {
+      dom.append('<li>' + GAME.players[i].name + ': ' + GAME.players[i].score + '</li>');
+    }
+  }
+}
 
-
+function displayCurrentPlayerTurn () {
+ 
+ let i = GAME.currentPlayerIndex;
+ 
+ $('#currentPlayer').text(GAME.players[i].name);
+ $("#playerTurnScore").text(GAME.players[i].turnScore);
+ $("#playerTotalScore").text(GAME.players[i].score);
+  //h2 #currentplayer
+//<span id="playerTurnScore"></span>
+//<span id="playerTotalScore"></span>
+}
 
 $(document).ready(function () {
+
+$('#settingsButton').click(function () {
+  $('#addPlayerForm').toggle();
+  $('#addNewPlayer').val('');
+});
+
+$("#addPlayerForm").submit(function() {
+  event.preventDefault();
+  const inputtedPlayer = $("input#addNewPlayer").val();
+  $("input#addNewPlayer").val("");
+  GAME.addPlayer(new Player(inputtedPlayer));
+  updatePlayerListScore ($('#playerScoreList'));
+  displayCurrentPlayerTurn();
+});
+
+$("#newGameButton").click(function () {
+  GAME.resetGame();
+  updatePlayerListScore ($('#playerScoreList'));
+  displayCurrentPlayerTurn();
+});
+
+$('#rollButton').click(function () {
+  DICE = GAME.rollDice();
+  $('#diceImg').attr('src', 'img/red_dice'+ DICE + '.png');
+  //display dice
+  if(GAME.turn()){
+    // if(GAME.player[GAME.currentPlayerIndex].submitTurnScore >= 100) {
+    //   alert('you won');
+    // }
+  }else{
+    GAME.nextPlayer();
+    
+  }
+  updatePlayerListScore ($('#playerScoreList'));
+  displayCurrentPlayerTurn();
+});
+$('#holdButton').click(function () {
+  GAME.players[GAME.currentPlayerIndex].submitTurnScore();
+  GAME.nextPlayer();
+  updatePlayerListScore ($('#playerScoreList'));
+  displayCurrentPlayerTurn();
+  //$('#holdButton').prop('disabled','true');
+
+});
+});
+
+
+/*
+  hold button should be able to be pressed before rolling/
+  change player background with player change/ add player background property
+  functionality for ending game at >= 100
+*/
+
+
 
 // addplayer button//////////
 // name form
@@ -104,4 +192,3 @@ $(document).ready(function () {
 // }
 
 
-});
